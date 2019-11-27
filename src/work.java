3a4ac3e759c05840c2c3f85d910163dc1fdf6d45
coldmilk <ssxoutlook@163.com>
2019-11-27 09:38:51 +0800

    Revert "测试"
    This reverts commit b8c1713c16869d31436e60ff3a248241b6840b49.

--- src/work.java
+++ src/work.java
@@ -2,17 +2,15 @@ public class work {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		int data1[][]=new int[50][4];
+		int data1[][]=new int[50][4];
 		
-//		for(int count1=0;count1<50;count1++) {
-//			int data2[][] = new create_equation().create();
-//			data1[count1][0]=data2[0][0];
-//			data1[count1][1]=data2[0][1];
-//			data1[count1][2]=data2[0][2];
-//			data1[count1][3]=data2[0][3];
-//		}
-
-		int data1[][] = new create_equation().create();
+		for(int count1=0;count1<50;count1++) {
+			int data2[][] = new create_equation().create();
+			data1[count1][0]=data2[0][0];
+			data1[count1][1]=data2[0][1];
+			data1[count1][2]=data2[0][2];
+			data1[count1][3]=data2[0][3];
+		}
 		
 		for(int count1=0;count1<50;count1++) {
 			System.out.print(data1[count1][0]+" "+(char)data1[count1][1]+" "+data1[count1][2]+" "+"="+data1[count1][3]+" ");
