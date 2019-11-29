e0dbe32d19fe205fc76dcb520a948f59a1b82f00
coldmilk <ssxoutlook@163.com>
2019-11-29 21:11:02 +0800

    更新

--- src/Software_Construction/Arithmetic_class_add.java
+++
@@ -1,7 +0,0 @@
-package Software_Construction;
-
-public class Arithmetic_class_add extends Arithmetic_class_abstract{
-	public int [][] Addition_Problem () {
-		return RandomCreate('+');
-	}
-}
--- src/Software_Construction/Arithmetic_class_subtraction.java
+++
@@ -1,7 +0,0 @@
-package Software_Construction;
-
-public class Arithmetic_class_subtraction extends Arithmetic_class_abstract{
-	public int [][] Addition_Problem () {
-		return RandomCreate('-');
-	}
-}
